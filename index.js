2702a9c02ce2db365f33c55cfb070638756cf926
Bamboozle-jpg <kevin@bachelorclan.com>
2022-08-09 22:01:13 -0700

    Reset happens automatically at 2am

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+
+//Tells it where the commands are
 for (const file of commandsFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
@@ -50,62 +52,90 @@ for (const file of commandsFiles) {
     client.commands.set(command.data.name, command);
 }
 
-//Tells console when bot is turned on
-client.on('ready', () => {
-    console.log("The Bot is Ready");
-});
-
+//Sets up channel we want to send stuff to
+const mainChannel = process.env.channelId;
 //Log in to bot
 client.login(process.env.TOKEN);
 
-client.on('messageCreate', (message) => {
-    var channel = message.channelId;
-    var guildString = process.env.guildId + '';
+function reset() {
+    //Then read people.json
+    fs.readFile("./people.json", "utf8", (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err);
+            return;
+        }
+        try {
 
-    //Make sure message isn't by a bot
-    if (!message.author.bot && message.content === '!reset') {
+            //Parse people.json
+            const people = JSON.parse(jsonString)
 
-        //Then read people.json
-        fs.readFile("./people.json", "utf8", (err, jsonString) => {
-            const channel = message.channelId
-            if (err) {
-                console.log("File read failed:", err);
-                return;
-            }
-            try {
+            client.channels.cache.get(mainChannel).send('**Workout bot\'s day has reset**');
+            //Resets everyone's score for the day
+            for (const person in people) {
+            
+                var userName = people[person][4];
 
-                //Parse people.json
-                const people = JSON.parse(jsonString)
-
-                //Resets everyone's score for the day
-                for (const person in people) {
-                
-                    var userName = people[person][4]
-
-                    //If they haven't done full workout today, kill full workout streak
-                    if (!people[person][2] && people[person][0]) {
-                        client.channels.cache.get(channel).send(userName+' lost their full streak of **'+people[person][0]+'**.');
-                        people[person][0] = 0;
-                    }
-
-                    //If they haven't done half workout today, kill half workout streak
-                    if (!people[person][3] && people[person][1]) {
-                        client.channels.cache.get(channel).send(userName+' lost their half streak of **'+people[person][1]+'**.');
-                        people[person][1] = 0;
-                    }
-
-                    people[person][2] = false;
-                    people[person][3] = false;
+                //If they haven't done full workout today, kill full workout streak
+                if (!people[person][2] && people[person][0]) {
+                    client.channels.cache.get(mainChannel).send(userName+' lost their full streak of **'+people[person][0]+'**.');
+                    people[person][0] = 0;
                 }
 
-                //Writes the day reset to file (rewrites file with new values)
-                fs.writeFile("./people.json", JSON.stringify(people, null, 4), err => {
-                    if (err) console.log("Error writing file:", err);
-                });
-            } catch (err) {
-                console.log("Error parsing JSON string:", err);
+                //If they haven't done half workout today, kill half workout streak
+                if (!people[person][3] && people[person][1]) {
+                    client.channels.cache.get(mainChannel).send(userName+' lost their half streak of **'+people[person][1]+'**.');
+                    people[person][1] = 0;
+                }
+
+                people[person][2] = false;
+                people[person][3] = false;
             }
-        });
+
+            //Writes the day reset to file (rewrites file with new values)
+            fs.writeFile("./people.json", JSON.stringify(people, null, 4), err => {
+                if (err) console.log("Error writing file:", err);
+            });
+        } catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+    });
+}
+
+function dateCheck(done) {
+    var d = new Date();
+    time = d.getUTCHours()
+    console.log('Still Running, time is : ' + d.toLocaleTimeString() + ' on ' + d.toLocaleDateString() + '. Done status : ' + done);
+    
+    //if it's 2am (time == 8) do the reset and stop it from resetting again
+    if (time == 8 && !done) {
+        reset();
+        console.log('done is true');
+        done = true;
+    }
+    //if it's 3am, tell it, it can reset again once it gets to 2am
+    if (time == 9 && done) {
+        console.log('done is false');
+        done = false
+    }
+
+    setTimeout(function(){dateCheck(done, d)}, 60000);
+}
+
+//Tells console when bot is turned on
+client.on('ready', () => {
+    console.log("\n\nThe Bot is Ready");
+    var d = new Date();
+    console.log(d.getUTCHours());
+    console.log(d.toLocaleDateString());
+    var done = false;
+    //Forever looping to check if the time is right
+    dateCheck(done)
+});
+
+client.on('messageCreate', (message) => {
+    //If a non-bot message is !reset, manually resets
+    if (!message.author.bot && message.content === '!reset') {
+        reset();
     }
 })
 
